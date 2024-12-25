2b69c9e771ed6e188fa6d0d0fa40c97694aaa6ca
m0rgi0n999 <m0rgi0n999@gmail.com>
2024-12-25 13:59:02 +0100

    sixth

--- src/components/MonsterList.js
+++ src/components/MonsterList.js
@@ -1,27 +1,34 @@
+-db/src/components/MonsterList.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const MonsterList = () => {
     const [monsters, setMonsters] = useState([]);
+    const [error, setError] = useState('');
 
     useEffect(() => {
         const fetchMonsters = async () => {
             try {
                 const response = await axios.get('http://localhost:3000/api/monsters');
                 setMonsters(response.data);
+                setError('');
             } catch (error) {
                 console.error('Error fetching monsters:', error);
+                setError('Failed to fetch monsters.');
             }
         };
         fetchMonsters();
     }, []);
 
     return (
-        <ul>
-            {monsters.map((monster) => (
-                <li key={monster.id}>{monster.name} - {monster.type} - {monster.hitPoints} HP</li>
-            ))}
-        </ul>
+        <div>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <ul>
+                {monsters.map((monster) => (
+                    <li key={monster.id}>{monster.name} - {monster.type} - {monster.hitPoints} HP</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
