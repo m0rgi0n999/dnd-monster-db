bba1fe79193d12d5c2f6bb240f035b86ee5dcc32
m0rgi0n999 <m0rgi0n999@gmail.com>
2024-12-25 09:43:42 +0100

    populate forms

--- src/components/MonsterForm.js
+++ src/components/MonsterForm.js
@@ -1,61 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const MonsterForm = ({ onAddMonster }) => {
-    const [name, setName] = useState('');
-    const [type, setType] = useState('');
-    const [hitPoints, setHitPoints] = useState('');
-    const [description, setDescription] = useState('');
+const MonsterList = () => {
+    const [monsters, setMonsters] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newMonster = { name, type, hitPoints, description };
-        onAddMonster(newMonster);
-        setName('');
-        setType('');
-        setHitPoints('');
-        setDescription('');
-    };
+    useEffect(() => {
+        const fetchMonsters = async () => {
+            const response = await axios.get('/api/monsters');
+            setMonsters(response.data);
+        };
+        fetchMonsters();
+    }, []);
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Name:</label>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label>Type:</label>
-                <input
-                    type="text"
-                    value={type}
-                    onChange={(e) => setType(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label>Hit Points:</label>
-                <input
-                    type="number"
-                    value={hitPoints}
-                    onChange={(e) => setHitPoints(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label>Description:</label>
-                <textarea
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Add Monster</button>
-        </form>
+        <ul>
+            {monsters.map((monster) => (
+                <li key={monster.id}>{monster.name} - {monster.type} - {monster.hitPoints} HP</li>
+            ))}
+        </ul>
     );
 };
 
-export default MonsterForm;
+export default MonsterList;
